24aed3b7013a8d827edf40a9f98ba1ad4529b346
Myint Myat Soe <max@max-mac.local>
2024-09-14 09:51:02 +0630

    put initialized code and build

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_toe/models/v_tic_tac_toe.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 1));
   runApp(GetMaterialApp(
     theme: ThemeData(useMaterial3: true),
     debugShowCheckedModeBanner: false,
